506c0b0880e9de474d20a2b38db6498bffa69e1a
Kashyap surani <152683782+Kashyapsurani@users.noreply.github.com>
2024-05-24 11:51:58 +0530

    Singup-form-javascript

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 var form = document.querySelector(".form")
-
 var arr = [];
 
 form.addEventListener("submit", () => {
+    var username = document.querySelector(".username").value
     var email = document.querySelector(".email").value
     var password = document.querySelector(".password").value
+
+
+    if (username.length < 4) {
+        var l = document.querySelector(".pargraph")
+        l.innerText = "You Username is smaller than 4"
+        return;
+
+    }
 
     if (password.length < 6) {
         var p = document.querySelector(".p")
@@ -16,6 +24,7 @@ form.addEventListener("submit", () => {
    
 
     var object = {
+        username: username,
         email: email,
         password : password
     }
